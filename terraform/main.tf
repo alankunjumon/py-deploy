3210e9c980dde69ab5b974f4f0957cf7cb3b56d5
alankunjumon <alankunjumon1305@gmail.com>
2025-12-24 04:18:04 +0000

    feat: setup grafana infrastructure and container

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,13 +50,20 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress{
+  ingress {
     description = "Prometheus UI"
     from_port = 9090
     to_port = 9090
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    description = "Grafana UI"
+    from_port = 3000
+    to_port = 3000
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
 
   egress{
     from_port = 0
